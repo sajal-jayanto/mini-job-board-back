f99198fbbf779576a68cb985257be2b884436bf7
sajal-jayanto <sajaljayanto007@gmail.com>
2025-07-23 18:30:46 +0600

    eslint update

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
